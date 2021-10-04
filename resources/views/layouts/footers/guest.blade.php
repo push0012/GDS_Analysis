33f6362ed1e32e584c7b30ceec2cd4dc644edeb2
Pushpamal Gunasena <pushpamal0012@gmail.com>
2021-10-04 19:41:17 +0530

    initializing the project

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="https://www.creative-tim.com">
+                {{ __('DCS (Planning) Office') }}
+            </a>
+            </li>
+           
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+        </div>
+    </div>
+</footer>
