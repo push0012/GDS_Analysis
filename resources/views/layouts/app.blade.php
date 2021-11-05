4b4be90f80bbdfd669be4487bfb0ffeae39a4496
push0012 <pushpamal0012@gmail.com>
2021-11-05 15:25:52 +0530

    add custom.js link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,6 +151,7 @@
         <!-- Material Dashboard DEMO methods, don't include it in your project! -->
         <script src="{{ asset('material') }}/demo/demo.js"></script>
         <script src="{{ asset('material') }}/js/settings.js"></script>
+        <script src="{{ asset('material') }}/js/custom.js"></script>
         @stack('js')
     </body>
 </html>
