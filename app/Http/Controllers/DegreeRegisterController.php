8de977a36cd9aa100e975f43bc8e85d72a0b58ee
push0012 <pushpamal0012@gmail.com>
2021-11-08 16:04:44 +0530

    add index function content

--- app/Http/Controllers/DegreeRegisterController.php
+++ app/Http/Controllers/DegreeRegisterController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Institute;
+use App\Models\Stream;
+use App\Models\Degree;
 
 class DegreeRegisterController extends Controller
 {
     public function index()
     {
-        return view('pages.degree.add');
+        $institutes = Institute::where('ins_type', 1)->get();
+        $streams = Stream::get();
+        $degrees = Degree::get();
+
+        return view('pages.degree.add', ['institutes' => $institutes, 'streams' => $streams, 'degrees' => $degrees ]);
     }
 }
