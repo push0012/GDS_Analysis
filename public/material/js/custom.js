bc8780ff510765fe6d4895327f95ccfeabe98c55
push0012 <pushpamal0012@gmail.com>
2021-11-08 16:01:09 +0530

    add content after line 8

--- public/material/js/custom.js
+++ public/material/js/custom.js
@@ -5,3 +5,29 @@ $(document).ready( function () {
         searching: false,
     } );
 } );
+jQuery(document).ready(function ()
+    {
+            jQuery('select[name="ds_id"]').on('change',function(){
+               var countryID = jQuery(this).val();
+               if(countryID)
+               {
+                  jQuery.ajax({
+                     url : '/divisions/' +countryID,
+                     type : "GET",
+                     dataType : "json",
+                     success:function(data)
+                     {
+                        console.log(data);
+                        jQuery('select[name="dv_id"]').empty();
+                        jQuery.each(data, function(key,value){
+                           $('select[name="dv_id"]').append('<option value="'+ key +'">'+ value +'</option>');
+                        });
+                     }
+                  });
+               }
+               else
+               {
+                  $('select[name="dv_id"]').empty();
+               }
+            });
+    });
