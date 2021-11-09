c51e094098e4e1e17578e5cbf2a851db50b4b5a4
push0012 <pushpamal0012@gmail.com>
2021-11-09 10:56:34 +0530

    doing changes and new additions

--- public/material/js/custom.js
+++ public/material/js/custom.js
@@ -7,10 +7,14 @@ $(document).ready( function () {
 } );
 jQuery(document).ready(function ()
     {
+      console.log("fuck here");
             jQuery('select[name="ds_id"]').on('change',function(){
+               console.log("here come");
                var countryID = jQuery(this).val();
+               console.log(countryID);
                if(countryID)
                {
+                  console.log(countryID);
                   jQuery.ajax({
                      url : '/divisions/' +countryID,
                      type : "GET",
@@ -31,3 +35,30 @@ jQuery(document).ready(function ()
                }
             });
     });
+
+function load_institute()
+{
+   console.log('call here');
+   jQuery.ajax({
+      url : '/ajax/institutes_load',
+      type : "GET",
+      dataType : "json",
+      success:function(data)
+      {
+         console.log(data);
+         jQuery('select[name="ins_id"]').empty();
+         $('select[name="ins_id"]').append('<option value="0" disabled="disabled" selected="selected">Pick One...</option>');
+         jQuery.each(data, function(key,value){
+            $('select[name="ins_id"]').append('<option value="'+ value.ins_id +'">'+ value.ins_name +'</option>');
+         });
+      },
+      error:function(data){
+         console.log("error here")
+      }
+      
+   });
+}
+
+function isMyFormValid(form){
+   console.log(form);
+}
