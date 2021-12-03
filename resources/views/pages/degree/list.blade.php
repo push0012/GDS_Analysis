359b1ce6fc7b9c54f32e1d7e2a3cef7fe874dc90
push0012 <pushpamal0012@gmail.com>
2021-12-03 14:03:10 +0530

    table font size changed

--- resources/views/pages/degree/list.blade.php
+++ resources/views/pages/degree/list.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <div class="content">
   <div class="container-fluid">
-      <div class="row " >
-            <div class="col-md-12 " align="right">
-             
-              <a href="{{ url('/register/graduate/create') }}" class="btn btn-primary">
-              
-                <span class="material-icons left">
-                add_circle
-                </span>
-                Add New
-                </a>
-              
-            </div>
-        </div>
-      <div class="row ">
+    <div class="row ">
+      <div class="col-md-12 " align="right">
+
+        <a href="{{ url('/register/graduate/create') }}" class="btn btn-primary">
+
+          <span class="material-icons left">
+            add_circle
+          </span>
+          Add New
+        </a>
+
+      </div>
+    </div>
+    <div class="row ">
       <div class="col-md-12">
         <div class="card card-plain">
           <div class="card-header card-header-primary">
@@ -25,7 +25,7 @@
           </div>
           <div class="card-body">
             <div class="table-responsive">
-              <table class="table table-hover stripe" id="myTabledeg">
+              <table class="table table-hover stripe" id="myTabledeg" style="font-size:0.85em">
                 <thead class="">
                   <th>
                     Reg No
@@ -45,19 +45,24 @@
                   <th>
                     View
                   </th>
+                  <th>
+                    View
+                  </th>
                 </thead>
                 <tbody>
-                @foreach($students as $key => $data)
-                    <tr>    
-                      <th>{{$data->deg_reg_no}}</th>
-                      <th>{{$data->deg_reg_date}}</th>
-                      <th>{{$data->stu_name}}</th>        
-                      <th>{{$data->nic}}</th>
-                      <th>{{$data->telephone}}</th>  
-                      <th><a href="{{ url('/register/graduate/view/'. $data->stu_id) }}"><span class="material-icons">visibility</span></a></th>      
-                    </tr>
-                @endforeach
-                 <!-- <tr>
+                  @foreach($students as $key => $data)
+                  <tr>
+                    <th>{{$data->deg_reg_no}}</th>
+                    <th>{{$data->deg_reg_date}}</th>
+                    <th>{{$data->stu_name}}</th>
+                    <th>{{$data->nic}}</th>
+                    <th>{{$data->telephone}}</th>
+                    <th><a href="{{ url('/register/graduate/view/'. $data->stu_id) }}"><span class="material-icons">visibility</span></a></th>
+                    <th><a href="{{ url('/register/graduate/view/'. $data->stu_id) }}"><span class="material-icons">visibility</span></a></th>
+
+                  </tr>
+                  @endforeach
+                  <!-- <tr>
                     <td>
                       1
                     </td>
