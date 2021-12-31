9a70cf1832f7d62cfcba7cdaac2dc791cefa0a7f
push0012 <pushpamal0012@gmail.com>
2021-12-31 09:44:55 +0530

    add danger session row before status session

--- resources/views/pages/degree/add.blade.php
+++ resources/views/pages/degree/add.blade.php
@@ -55,6 +55,18 @@
                 
               </div>
               <div class="card-body ">
+              @if (session('danger'))
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <i class="material-icons">close</i>
+                        </button>
+                        <span>{{ session('danger') }}</span>
+                      </div>
+                    </div>
+                  </div>
+                @endif
                 @if (session('status'))
                   <div class="row">
                     <div class="col-sm-12">
